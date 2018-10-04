67477cc556a47c01809cce421f30495c7275cfe7
Kristof Coomans <kristof@2dotstwice.be>
2018-10-04 10:30:59 +0200

    III-2701: First read from the repository before calling culturefeed

--- src/CultureFeed/CultureFeedApiKeyAuthenticator.php
+++ src/CultureFeed/CultureFeedApiKeyAuthenticator.php
@@ -5,6 +5,7 @@ namespace CultuurNet\UDB3\ApiGuard\CultureFeed;
 use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
 use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKeyAuthenticationException;
 use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKeyAuthenticatorInterface;
+use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerReadRepositoryInterface;
 use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerWriteRepositoryInterface;
 
 class CultureFeedApiKeyAuthenticator implements ApiKeyAuthenticatorInterface
@@ -20,23 +21,32 @@ class CultureFeedApiKeyAuthenticator implements ApiKeyAuthenticatorInterface
     private $consumerWriteRepository;
 
     /**
+     * @var ConsumerReadRepositoryInterface
+     */
+    private $consumerReadRepository;
+
+    /**
      * @var bool
      */
     private $includePermissions;
 
     /**
      * @param \ICultureFeed $cultureFeed
+     * @param ConsumerReadRepositoryInterface
      * @param ConsumerWriteRepositoryInterface $consumerWriteRepository
      *   CultureFeed returns a consumer detail while authenticating.
      *   This consumer will be written to the injected write repository as a
      *   form of caching.
+     * @param bool $includePermissions
      */
     public function __construct(
         \ICultureFeed $cultureFeed,
+        ConsumerReadRepositoryInterface $consumerReadRepository,
         ConsumerWriteRepositoryInterface $consumerWriteRepository,
         $includePermissions = false
     ) {
         $this->cultureFeed = $cultureFeed;
+        $this->consumerReadRepository = $consumerReadRepository;
         $this->consumerWriteRepository = $consumerWriteRepository;
         $this->includePermissions = $includePermissions;
     }
@@ -48,6 +58,12 @@ class CultureFeedApiKeyAuthenticator implements ApiKeyAuthenticatorInterface
     public function authenticate(ApiKey $apiKey)
     {
         try {
+            $consumer = $this->consumerReadRepository->getConsumer($apiKey);
+            
+            if ($consumer) {
+                return;
+            }
+            
             $consumer = $this->cultureFeed->getServiceConsumerByApiKey(
                 $apiKey->toNative(),
                 $this->includePermissions
---
+++ src/Doctrine/ConsumerRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace CultuurNet\UDB3\ApiGuard\Doctrine;
+
+use CultuurNet\UDB3\ApiGuard\ApiKey\ApiKey;
+use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerInterface;
+use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerReadRepositoryInterface;
+use CultuurNet\UDB3\ApiGuard\Consumer\ConsumerWriteRepositoryInterface;
+use Doctrine\Common\Cache\Cache;
+
+class ConsumerRepository implements ConsumerReadRepositoryInterface, ConsumerWriteRepositoryInterface
+{
+    /**
+     * @var Cache
+     */
+    private $cache;
+
+    /**
+     * @var int
+     */
+    private $lifeTime;
+
+    /**
+     * ConsumerRepository constructor.
+     * @param \Doctrine\Common\Cache\Cache $cache
+     * @param int $lifeTime
+     *  The lifetime in number of seconds for cache entries.
+     */
+    public function __construct(Cache $cache, $lifeTime = 86400)
+    {
+        $this->cache = $cache;
+    }
+
+    public function getConsumer(ApiKey $apiKey)
+    {
+        return $this->cache->fetch($apiKey->toNative());
+    }
+
+    public function setConsumer(ApiKey $apiKey, ConsumerInterface $consumer)
+    {
+        $this->cache->save($apiKey->toNative(), $consumer, $this->lifeTime);
+    }
+}
