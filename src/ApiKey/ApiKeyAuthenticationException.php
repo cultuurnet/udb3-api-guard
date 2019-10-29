22c1a5cb651a5f791cce786e86ca3c40ac5231d5
zvonimir <zspajich@gmail.com>
2019-10-29 15:05:21 +0100

    Add guard against invalid consumer status

--- src/ApiKey/ApiKeyAuthenticationException.php
+++ src/ApiKey/ApiKeyAuthenticationException.php
@@ -4,4 +4,23 @@ namespace CultuurNet\UDB3\ApiGuard\ApiKey;
 
 class ApiKeyAuthenticationException extends \Exception
 {
+    public static function cultureFeedErrorForKey(ApiKey $apiKey): ApiKeyAuthenticationException
+    {
+        return new self(self::messageForKey($apiKey));
+    }
+
+    public static function keyBlocked(ApiKey $apiKey): ApiKeyAuthenticationException
+    {
+        return new self(self::messageForKey($apiKey) . ' Key is blocked');
+    }
+
+    public static function keyRemoved(ApiKey $apiKey): ApiKeyAuthenticationException
+    {
+        return new self(self::messageForKey($apiKey) . ' Key is removed');
+    }
+
+    private static function messageForKey(ApiKey $apiKey): string
+    {
+        return 'Could not authenticate with API key "' . $apiKey->toNative() . '".';
+    }
 }
