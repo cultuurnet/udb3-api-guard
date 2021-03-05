7f7e34a450c1bf5b3d757cb9494503bcaea18763
Jonas Drieghe <jonasd@madewithlove.be>
2021-03-05 07:46:54 +0100

    Internalize value object abstraction into its only extension

--- src/ApiKey/ApiKey.php
+++ src/ApiKey/ApiKey.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace CultuurNet\UDB3\ApiGuard\ApiKey;
 
-use CultuurNet\UDB3\ApiGuard\ValueObjects\StringLiteral;
-
-final class ApiKey extends StringLiteral
+final class ApiKey
 {
+    /**
+     * @var string
+     */
+    protected $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    public function toNative(): string
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->toNative();
+    }
 }
